30e51179d846c6279cca8fb8915406baa0b3ab97
Fabian Neumann <fabian.neumann@outlook.de>
2020-03-27 10:30:41 +0100

    solve: split bidirectional links for loss considerations

--- scripts/solve_network.py
+++ scripts/solve_network.py
@@ -12,6 +12,7 @@ import os
 import sys
 
 from vresutils.benchmark import memory_logger
+from pypsa.opt import l_constraint
 
 import logging
 
@@ -29,6 +30,17 @@ from solve_network import *
 pypsa.pf.logger.setLevel(logging.WARNING)
 
 
+def split_bidirectional_links(n):
+    
+    n.links.p_min_pu = 0
+    rev_links = n.links.copy().rename({"bus0": "bus1", "bus1": "bus0"}, axis=1)
+    rev_links.capital_cost = 0
+    rev_links["reversed"] = True
+    rev_links.index = [f"{i}-reversed" for i in rev_links.index]
+
+    n.links = pd.concat([n.links, rev_links], sort=False)
+
+
 def remove_kvl_constraints(network, snapshots):
 
     formulation = snakemake.config["solving"]["options"].get("formulation", "kirchhoff")
@@ -38,6 +50,26 @@ def remove_kvl_constraints(network, snapshots):
 
     if formulation in ["cycles", "kirchhoff"]:
         n.model.del_component(network.model.cycle_constraints)
+
+
+def tie_bidirectional_link_p_nom(network, snapshots):
+    
+    if not hasattr(n.links, 'reversed'): return
+    
+    ext_rev_links = network.links.loc[
+                        (network.links.reversed==True) & 
+                        (network.links.p_nom_extendable==True)
+                    ].index
+
+    if len(ext_rev_links) == 0: return
+    
+    constraints = {lk :
+            [[(1,network.model.link_p_nom[lk.split("-")[0]]),
+              (-1,network.model.link_p_nom[lk])
+             ],"==",0.]
+        for lk in ext_rev_links}
+    
+    l_constraint(network.model, "bidirectional_link", constraints, list(ext_rev_links))
 
 
 if __name__ == "__main__":
@@ -63,39 +95,48 @@ if __name__ == "__main__":
 
         n = pypsa.Network(snakemake.input[0])
 
-        # network adjustments
-        n.links.p_nom_max = config["links_p_nom_max"]
+        lk_config = config["links"]
         n.lines.s_nom_max = n.lines.apply(
             lambda line: max(
-                line.s_nom + config["additional_s_nom"],
-                line.s_nom * config["s_nom_factor"],
+                line.s_nom + lk_config["s_nom_add"],
+                line.s_nom * lk_config["s_nom_factor"],
             ),
             axis=1
         )
         n.lines = n.lines.loc[n.lines.s_nom != 0]
 
+        lk_config = config["lines"]
+        n.links.p_nom_max = lk_config["p_nom_max"]
+        n.links.efficiency = n.links.apply(lambda lk: 1 - lk.length * lk_config["efficiency_per_length"], axis=1)
+        split_bidirectional_links(n)
+
         n = prepare_network(n, solve_opts=snakemake.config["solving"]["options"])
 
-        # set extra_functionality, extra_postprocessing, iterating
+        # set iterating
         if model == "transport":
-            extra_functionality = remove_kvl_constraints
-            extra_postprocessing = None
             skip_iterating = True
         elif model == "lossy":
             n.tangents = int(model_wc[1])
-            extra_functionality = define_loss_constraints
-            extra_postprocessing = store_losses
             skip_iterating = False
         elif model == "lossless":
-            extra_functionality = None
-            extra_postprocessing = None
             iterations = int(model_wc[1])
             if iterations == 0:
                 skip_iterating = True
             else:
                 skip_iterating = False
+                snakemake.config["solving"]["options"]["min_iterations"] = iterations - 1
                 snakemake.config["solving"]["options"]["max_iterations"] = iterations
-                snakemake.config["solving"]["options"]["min_iterations"] = iterations
+
+        def extra_functionality(network, snapshots):
+            tie_bidirectional_link_p_nom(network, snapshots)
+            if model == "transport":
+                remove_kvl_constraints(netowrk, snapshots)
+            if model == "lossy"
+                define_loss_constraints(network, snapshots)
+
+        def extra_postprocessing(network, snapshots, duals):
+            if model == "lossy":
+                store_losses(network, snapshots, duals)
 
         n = solve_network(
             n,
